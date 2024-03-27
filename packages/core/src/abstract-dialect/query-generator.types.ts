543568fcc461362ed50d5c7b4dedad300618aa69
lohart13 <lohart13@gmail.com>
2024-03-28 12:12:35 +1300

    feat: migrate index queries to ts

--- packages/core/src/abstract-dialect/query-generator.types.ts
+++ packages/core/src/abstract-dialect/query-generator.types.ts
@@ -12,7 +12,7 @@ import type {
 } from '../temporal-tables';
 import type { TransactionType } from '../transaction';
 import type { AddLimitOffsetOptions } from './query-generator.internal-types.js';
-import type { TableName } from './query-interface.js';
+import type { IndexOptions, TableName } from './query-interface.js';
 import type { ConstraintType } from './query-interface.types';
 import type { WhereOptions } from './where-sql-builder-types';
 
@@ -201,6 +201,18 @@ export interface ShowConstraintsQueryOptions {
   constraintType?: ConstraintType;
 }
 
+// keep ADD_INDEX_QUERY_SUPPORTABLE_OPTIONS updated when modifying this
+export interface AddIndexQueryOptions extends Omit<IndexOptions, 'msg'> {
+  ifNotExists?: boolean;
+}
+
+// keep REMOVE_INDEX_QUERY_SUPPORTABLE_OPTIONS updated when modifying this
+export interface RemoveIndexQueryOptions {
+  concurrently?: boolean;
+  ifExists?: boolean;
+  cascade?: boolean;
+}
+
 // keep START_TRANSACTION_QUERY_SUPPORTABLE_OPTIONS updated when modifying this
 export interface StartTransactionQueryOptions {
   readOnly?: boolean;
@@ -217,10 +229,3 @@ export interface QuoteTableOptions extends IndexHintable {
 export interface BulkDeleteQueryOptions<TAttributes = any>
   extends AddLimitOffsetOptions,
     Filterable<TAttributes> {}
-
-// keep REMOVE_INDEX_QUERY_SUPPORTABLE_OPTIONS updated when modifying this
-export interface RemoveIndexQueryOptions {
-  concurrently?: boolean;
-  ifExists?: boolean;
-  cascade?: boolean;
-}
